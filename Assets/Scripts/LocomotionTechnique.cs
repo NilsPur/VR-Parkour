fdb3a5c66fb0617d599a9d0741f01d1da8d7bfa5
NilsPur <48684538+NilsPur@users.noreply.github.com>
2023-01-07 14:29:57 +0100

    teleport change "movement" curve

--- Assets/Scripts/LocomotionTechnique.cs
+++ Assets/Scripts/LocomotionTechnique.cs
@@ -4,6 +4,7 @@ using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.UI.Image;
 
 [RequireComponent(typeof(AudioSource))]
 public class LocomotionTechnique : MonoBehaviour
@@ -26,17 +27,16 @@ public class LocomotionTechnique : MonoBehaviour
     [SerializeField] private Vector3 startPos;
     [SerializeField] private Vector3 offset;
     [SerializeField] private bool isIndexTriggerDown;
+    [SerializeField] private LineDrawer lineDrawerLeftHand;
+    [SerializeField] private LineDrawer lineDrawerRightHand;
     private AudioSource audioSource;
-
-    /////////////////////////////////////////////////////////
-    // These are for the game mechanism.
+    private LineDrawer[] lineDrawers = new LineDrawer[2];
     public ParkourCounter parkourCounter;
     public string stage;
     public SelectionTaskMeasure selectionTaskMeasure;
     public Hand[] hands { get; set; } = new Hand[2];
     public Grabber[] grabbers { get; set; } = new Grabber[2];
     public bool[] TeleportationStarted { get; set; } = new bool[2];
-    public Pose[] StartTeleportationPose { get; set; } = new Pose[2];
 
     void Start()
     {
@@ -45,38 +45,12 @@ public class LocomotionTechnique : MonoBehaviour
         hands[1] = rightHand;
         grabbers[0] = leftGrabber;
         grabbers[1] = rightGrabber;
+        lineDrawers[0] = lineDrawerLeftHand;
+        lineDrawers[1] = lineDrawerRightHand;
     }
 
     void Update()
-    {
-        //Debug.Log("L-hand scale: " + leftHand.HandScale);
-        //Debug.Log("R-hand scale: " + rightHand.HandScale);
-
-        //OVRBone[] leftHandBones = leftHandSkeleton.Bones.ToArray();
-        //OVRBone[] rightHandBones = rightHandSkeleton.Bones.ToArray();
-
-        //Debug.Log("L-hand Bones (Local): " + string.Join<Vector3>(", ", Array.ConvertAll(leftHandBones, bone => bone.Transform.localPosition)));
-        //Debug.Log("R-hand Bones (Local): " + string.Join<Vector3>(", ", Array.ConvertAll(rightHandBones, bone => bone.Transform.localPosition)));
-
-        //Debug.Log("L-hand Bones (Global): " + string.Join<Vector3>(", ", Array.ConvertAll(leftHandBones, bone => bone.Transform.position)));
-        //Debug.Log("R-hand Bones (Global): " + string.Join<Vector3>(", ", Array.ConvertAll(rightHandBones, bone => bone.Transform.position)));
-
-        //Debug.Log("L-hand Bones (Rotation): " + string.Join<Quaternion>(", ", Array.ConvertAll(leftHandBones, bone => bone.Transform.localRotation)));
-        //Debug.Log("R-hand Bones (Rotation): " + string.Join<Quaternion>(", ", Array.ConvertAll(rightHandBones, bone => bone.Transform.localRotation)));
-
-        //Debug.Log("L-hand:");
-        //PrintPinch(leftHand);
-        //Debug.Log("R-hand:");
-        //PrintPinch(rightHand);
-
-        //Debug.Log("Active Controller: " + OVRInput.GetActiveController());
-        //if (OVRInput.GetActiveController() != OVRInput.Controller.None)
-        //{
-
-        //}
-
-        //Debug.Log("Connected Controller: " + OVRInput.GetConnectedControllers());
-        
+    {        
 		for (int i = 0; i < hands.Length; i++)
         {
             if (hands[i].IsHighConfidence)
@@ -100,8 +74,8 @@ public class LocomotionTechnique : MonoBehaviour
                     else if (!TeleportationStarted[i])
                     {
                         hands[i].GetRootPose(out Pose handPose);
-                        StartTeleportationPose[i] = handPose;
-                        TeleportationStarted[i] = true;
+						lineDrawers[i]?.FixateEndPoint(handPose.position);
+						TeleportationStarted[i] = true;
                     }
                 }
                 else
@@ -116,88 +90,13 @@ public class LocomotionTechnique : MonoBehaviour
                     }
                     else if (TeleportationStarted[i])
                     {
-                        TeleportToHandPosition(hands[i], StartTeleportationPose[i]);
-                        hands[i].GetRootPose(out Pose handPose);
-                        Vector3 offset = StartTeleportationPose[i].position - handPose.position;
-
-                        TeleportationStarted[i] = false;
+						lineDrawers[i].ReleaseFixedEndpoint();
+						TeleportToHandPosition(i);
+						TeleportationStarted[i] = false;
                     }
                 }
             }
 		}
-
-        //switch (OVRInput.GetActiveController())
-        //{
-        //    case OVRInput.Controller.LTouch:
-        //    case OVRInput.Controller.RTouch:
-        //    case OVRInput.Controller.Touch:
-        //        break;
-
-        //    case OVRInput.Controller.Hands:
-        //    case OVRInput.Controller.RHand:
-        //    case OVRInput.Controller.LHand:
-        //        break;
-        //}
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        //// Please implement your LOCOMOTION TECHNIQUE in this script :D.
-        //leftTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, leftController);
-        //rightTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, rightController);
-
-        //if (leftTriggerValue > 0.95f && rightTriggerValue > 0.95f)
-        //{
-        //    if (!isIndexTriggerDown)
-        //    {
-        //        isIndexTriggerDown = true;
-        //        startPos = (OVRInput.GetLocalControllerPosition(leftController) + OVRInput.GetLocalControllerPosition(rightController)) / 2;
-        //    }
-        //    offset = hmd.transform.forward.normalized *
-        //            ((OVRInput.GetLocalControllerPosition(leftController) - startPos) +
-        //             (OVRInput.GetLocalControllerPosition(rightController) - startPos)).magnitude;
-        //    Debug.DrawRay(startPos, offset, Color.red, 0.2f);
-        //}
-        //else if (leftTriggerValue > 0.95f && rightTriggerValue < 0.95f)
-        //{
-        //    if (!isIndexTriggerDown)
-        //    {
-        //        isIndexTriggerDown = true;
-        //        startPos = OVRInput.GetLocalControllerPosition(leftController);
-        //    }
-        //    offset = hmd.transform.forward.normalized *
-        //             (OVRInput.GetLocalControllerPosition(leftController) - startPos).magnitude;
-        //    Debug.DrawRay(startPos, offset, Color.red, 0.2f);
-        //}
-        //else if (leftTriggerValue < 0.95f && rightTriggerValue > 0.95f)
-        //{
-        //    if (!isIndexTriggerDown)
-        //    {
-        //        isIndexTriggerDown = true;
-        //        startPos = OVRInput.GetLocalControllerPosition(rightController);
-        //    }
-        //    offset = hmd.transform.forward.normalized *
-        //             (OVRInput.GetLocalControllerPosition(rightController) - startPos).magnitude;
-        //    Debug.DrawRay(startPos, offset, Color.red, 0.2f);
-        //}
-        //else
-        //{
-        //    if (isIndexTriggerDown)
-        //    {
-        //        isIndexTriggerDown = false;
-        //        offset = Vector3.zero;
-        //    }
-        //}
-        //this.transform.position = this.transform.position + (offset) * translationGain;
-
-
-        //////////////////////////////////////////////////////////////////////////////////
-        //// These are for the game mechanism.
-        //if (OVRInput.Get(OVRInput.Button.Two) || OVRInput.Get(OVRInput.Button.Four))
-        //{
-        //    if (parkourCounter.parkourStart)
-        //    {
-        //        this.transform.position = parkourCounter.currentRespawnPos;
-        //    }
-        //}
     }
 
     void OnTriggerEnter(Collider other)
@@ -230,45 +129,24 @@ public class LocomotionTechnique : MonoBehaviour
         // These are for the game mechanism.
     }
 
-    private void PrintPinch(OVRHand hand)
-    {
-        for (int fingerIndex = (int)OVRHand.HandFinger.Thumb; fingerIndex < (int)OVRHand.HandFinger.Max; fingerIndex++)
-        {
-            OVRHand.HandFinger finger = (OVRHand.HandFinger)fingerIndex;
-            Debug.Log(finger.ToString() + " (Pinch): " + hand.GetFingerPinchStrength(finger));
-        }
-    }
-
     public void SwitchToCalibrationScene()
     {
         SceneManager.LoadScene("Calibration");
     }
 
-    //public void Fist()
-    //{
-    //    Debug.Log("Fist");
-    //    TeleportToHandPosition(rightHand);
-    //}
-
     private RaycastHit[] hits = new RaycastHit[20];
     const int rayY = 200;
 
-    private void TeleportToHandPosition(Hand hand, Pose startHandPose)
+    private void TeleportToHandPosition(int handIndex)
     {
+        Hand hand = hands[handIndex];
         if (hand.GetRootPose(out Pose handPose) && hmd.GetRootPose(out Pose hmdPose))
         {
             float newRigRotation = (360 + transform.InverseTransformDirection(0, handPose.rotation.eulerAngles.y, 0).y - 90 - hmdPose.rotation.eulerAngles.y) % 360;
-            Debug.LogWarning("hand rotation: " + handPose.rotation.eulerAngles);
-            Debug.LogWarning("hmd rotation: " + hmdPose.rotation.eulerAngles);
-            Debug.LogWarning("camera rig rotation: " + transform.rotation.eulerAngles);
-
-            //float armRotation = Quaternion.LookRotation(handPose.position - hmdPose.transform.position, Vector3.up).eulerAngles.y - hmdPose.rotation.eulerAngles.y;
-            //float handRotation = transform.InverseTransformDirection(0, handPose.rotation.eulerAngles.y, 0).y - 90 - armRotation;
-            //float newRigRotation = (720 + hmdPose.rotation.eulerAngles.y + handRotation) % 360;
-            //Debug.Assert(t == newRigRotation);
-
-            //Debug.Log("arm rotation: " + armRotation);
-            Debug.LogWarning("new camera rig rotation: " + newRigRotation);
+            //Debug.LogWarning("hand rotation: " + handPose.rotation.eulerAngles);
+            //Debug.LogWarning("hmd rotation: " + hmdPose.rotation.eulerAngles);
+            //Debug.LogWarning("camera rig rotation: " + transform.rotation.eulerAngles);
+            //Debug.LogWarning("new camera rig rotation: " + newRigRotation);
 
             float newY = transform.position.y;
             if (Physics.Raycast(handPose.position + rayY * Vector3.up, Vector3.down, out RaycastHit hit, 2 * rayY, LayerMask.GetMask("Terrain")))
@@ -278,26 +156,16 @@ public class LocomotionTechnique : MonoBehaviour
 
             Vector3 newPosition = new Vector3(handPose.position.x, newY, handPose.position.z);
             Quaternion newOrientation = Quaternion.Euler(transform.rotation.eulerAngles.x, newRigRotation, transform.rotation.eulerAngles.z);
-            Vector3 direction = newPosition - transform.position;
+            
+            // collect everything in a straight line from current position to new position
+			Vector3 origin = transform.position + new Vector3(0, 1.5f, 0); // add some height so the ray is able to hit coins
+            bool coinHit = CastRay(origin, newPosition - transform.position);
 
-            bool coinHit = false;
-            Vector3 origin = transform.position + new Vector3(0, 1.5f, 0); // add some height so the ray is able to hit coins
-            int hitCount = Physics.RaycastNonAlloc(origin, direction, hits, direction.magnitude, LayerMask.GetMask("locomotion"));
-            for (int i = 0; i < hitCount; i++)
+            // collect everything on the path of the curve indicator
+			Vector3[] points = lineDrawers[handIndex]?.GetPoints() ?? Array.Empty<Vector3>();
+            for (int i = 0; i < points.Length - 1; i++)
             {
-                GameObject hitObject = hits[i].collider.gameObject;
-
-                if (hitObject.CompareTag("coin"))
-                {
-                    parkourCounter.coinCount++;
-                    hitObject.SetActive(false);
-                    coinHit = true;
-                }
-                else if (hitObject.CompareTag("banner"))
-                {
-                    stage = hitObject.gameObject.name;
-                    parkourCounter.isStageChange = true;
-                }
+                coinHit |= CastRay(points[i], points[i + 1] - points[i]);
             }
             if (coinHit)
             {
@@ -311,4 +179,26 @@ public class LocomotionTechnique : MonoBehaviour
             Debug.Log("Positioning failed");
         }
     }
+    private bool CastRay(Vector3 origin, Vector3 direction)
+    {
+        bool coinHit = false;
+        int hitCount = Physics.RaycastNonAlloc(origin, direction, hits, direction.magnitude, LayerMask.GetMask("locomotion"));
+		for (int i = 0; i < hitCount; i++)
+		{
+			GameObject hitObject = hits[i].collider.gameObject;
+
+			if (hitObject.CompareTag("coin"))
+			{
+				parkourCounter.coinCount++;
+				hitObject.SetActive(false);
+				coinHit = true;
+			}
+			else if (hitObject.CompareTag("banner"))
+			{
+				stage = hitObject.name;
+				parkourCounter.isStageChange = true;
+			}
+		}
+        return coinHit;
+	}
 }
